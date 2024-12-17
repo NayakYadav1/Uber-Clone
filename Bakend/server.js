c8a00ac477f6d1dc0d830f0d1faf955ac808858e
NayakYadav1 <yadavnayak202@gmail.com>
2024-12-17 15:09:45 +0545

    Register User

--- Bakend/server.js
+++ Bakend/server.js
@@ -1,9 +1,10 @@
 const http = require('http');
 const app = require('./app');
+const { default: mongoose, mongo } = require('mongoose');
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
-server.listen(port, () => {
+server.listen(port, async() => {
     console.log(`Server successfully started on port ${port}`); 
 });
